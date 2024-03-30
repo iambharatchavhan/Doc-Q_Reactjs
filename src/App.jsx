1864aad11f88043931a1bb227af219cb59e6aac4
vasanshettiTharun <vasanshettitharun@gmail.com>
2024-03-30 15:42:42 +0530

    Desktop_14_page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import FindADoctor from "./components/Find_a_doctors_page/navbar/Find_a_docter_page";
+// import FindADoctor from "./components/Find_a_doctors_page/navbar/Find_a_docter_page";
+import Desktop14 from "./components/Desktop-14/Desktop14/Desktop14"
 
 function App() {
   return (
     <div>
-    <FindADoctor/>
+    {/* <FindADoctor/> */}
+    <Desktop14/>
     </div>
   );
 } 
