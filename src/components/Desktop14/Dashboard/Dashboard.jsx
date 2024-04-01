cb2d0ef959309c2ee07cf1b764510a420ad5f8fc
vasanshettiTharun <vasanshettitharun@gmail.com>
2024-04-01 21:51:08 +0530

    desktop_14_dashboard

--- src/components/Desktop14/Dashboard/Dashboard.jsx
+++ src/components/Desktop14/Dashboard/Dashboard.jsx
@@ -6,8 +6,10 @@ import Frame53 from "./Frame53";
 
 function Dashboard() {
   return (
-    <div className="w-full border bg-[#FAFAFA]">
+    <div className='w-full mt-[8vh] md:mt-[5vh] lg:mt-[12vh] bg-[#FAFAFA] h-auto lg:h-screen max-w-[80%] md:max-w-[90%] lg:max-w-[85%] p-2 lg:p-5'>
+      <div>
     <Frame49 />
+      </div>
     <div className="max-w-full h-auto  m-2">
       <Threetabs />
     </div>
@@ -19,4 +21,4 @@ function Dashboard() {
   )
 }
 
-export default Dashboard
+export default Dashboard;
