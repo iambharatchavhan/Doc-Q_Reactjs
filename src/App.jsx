f01ab2d078a84a577d538fe193caedd797e621f6
Dasari Manoj <122810482+Manudasari265@users.noreply.github.com>
2024-04-02 12:42:36 +0530

    Doctor profile added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { Outlet } from "react-router-dom";
-import Header from "./components/Header/Header";
-import Sidebar from "./components/Sidebar/Sidebar";
+// import { Outlet } from "react-router-dom";
+// import Header from "./components/Header/Header";
+// import Sidebar from "./components/Sidebar/Sidebar";
+import DoctorProfile from "./components/Doctorprofile/Doctorprofile";
 
 function App() {
   return (
     <>
-      <Header />
-      <div className="flex justify-end">
-        <Sidebar />
-        <Outlet />
-      </div>
+      {/* <Header /> */}
+      {/* <div className="flex justify-end"> */}
+        {/* <Sidebar />
+        <Outlet /> */}
+        <DoctorProfile/>
+      {/* </div> */}
     </>
   );
 }
